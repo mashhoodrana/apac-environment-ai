8ea4b32eb00d62e507167307bbeb9171f3bcfc8b
Mashhood Rana <147306863+mashhoodrana@users.noreply.github.com>
2025-05-16 23:29:53 +0500

    Delete app directory

--- app/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Environmental Analysis API
-""" 
--- app/services/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Service layer for external data sources
-"""
-from . import gee_service
-from . import chatbot_service
-from . import cloud_storage
-from . import model_service 
